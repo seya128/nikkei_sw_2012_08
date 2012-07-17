e7e6c5315dcfca088512003afc4f4b18f9b4b563
Shinichi Seya <shinichi.seya@gmail.com>
2012-07-17 09:14:01 +0900

    js：リスト３をそのまま追加してツイートが円状に飛び散るようになった

--- js/game.js
+++ js/game.js
@@ -10,9 +10,42 @@ window.onload = function() {
 
     var tweets = game.twitterAssets["statuses/home_timeline"];
 
+    var count = 0;
+
+    var wally = tweets[0].user.screen_name;
+
     tweets.forEach(function(tweet) {
 
       var user = tweet.user.toSprite();
+
+      if (tweet.user.screen_name !== wally) {
+        count += 1;
+        user.addEventListener(Event.TOUCH_START, function() {
+          game.rootScene.removeChild(user);
+          count -= 1;
+          if (count <= 0) {
+            game.end(1 / tl.time, tl.time + "秒で見つけた");
+            return;
+          }
+          var speed = 5;
+
+          tweet.text.split("").forEach(function(chr, i) {
+            var lab = new Label(chr);
+            lab.x = user.x;
+            lab.y = user.y;
+
+            var theta = 2 * Math.PI * i / tweet.text.length;
+            var vx = speed * Math.cos(theta);
+            var vy = speed * Math.sin(theta);
+            lab.addEventListener(Event.ENTER_FRAME, function() {
+              lab.x += vx;
+              lab.y += vy;
+            });
+            game.rootScene.addChild(lab);
+          });
+        });
+      }
+
       user.scaleX = user.scaleY = 0.5 + Math.random();
       user.x = (320 - user.width) * Math.random();
       user.y = (320 - user.height) * Math.random();
